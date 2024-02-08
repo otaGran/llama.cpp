a7ca49f3cec289d9f254d2478737523d5529b108
otaGran <mba@>
2024-02-07 23:40:37 -0500

    do not apply embd

--- llama.cpp
+++ llama.cpp
@@ -7036,7 +7036,7 @@ static struct ggml_cgraph * llama_build_graph(
 
         }
 
-        if (batch.embd || false) {
+        if (batch.embd && false) {
             //
             const int64_t n_embd   = llm.n_embd;
             bool sec = false;
